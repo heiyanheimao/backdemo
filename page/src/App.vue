4ed67fef6086ae93a3090ca909aeb8b4ecc91909
heiyanheimao <550715038@qq.com>
2019-11-29 18:07:38 +0800

    test

--- page/src/App.vue
+++ page/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <el-button @click="show = !show">Click Me</el-button>
-
+    <!-- <el-button @click="show = !show">Click Me</el-button>
     <div style="display: flex; margin-top: 20px; height: 100px;">
       <transition name="el-fade-in-linear">
         <div v-show="show" class="transition-box">.el-fade-in-linear</div>
@@ -9,27 +8,21 @@
       <transition name="el-fade-in">
         <div v-show="show" class="transition-box">.el-fade-in</div>
       </transition>
-    </div>
+    </div>-->
+    <router-link to="/test">test</router-link>
+    <router-view></router-view>
   </div>
 </template>
 <script>
 export default {
   data: () => ({
     show: true
-  })
+  }),
+  methods: {
+  },
+  created () {
+  }
 }
 </script>
 <style>
-.transition-box {
-  margin-bottom: 10px;
-  width: 200px;
-  height: 100px;
-  border-radius: 4px;
-  background-color: #409EFF;
-  text-align: center;
-  color: #fff;
-  padding: 40px 20px;
-  box-sizing: border-box;
-  margin-right: 20px;
-}
 </style>
--- page/src/router/index.js
+++ page/src/router/index.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
+const Test = () => import('views/test/Test')
 const routes = [
+  {
+    path: '/test',
+    component: Test
+  }
 ]
 
 const router = new VueRouter({
